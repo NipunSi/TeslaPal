cfd640146468022bb1fc92cfb4609c67c79c8f25
Nipun Singh <nipunbusiness@gmail.com>
2020-10-14 14:01:01 -0700

    Initial Commit

---
+++ TeslaHelper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TeslaHelper
+//
+//  Created by Nipun Singh on 10/14/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
